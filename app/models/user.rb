a9e51f2a4d75ed6f56aa0b86838290cdc895caf4
vagrant <vagrant@localhost.localdomain>
2020-07-05 02:32:18 +0000

    [finish]task5

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,25 @@ class User < ApplicationRecord
     self.followings.include?(other_user)
   end
 
+  def self.search(search, word)
+      if search == "forward_match"
+             @user = User.where("name LIKE?", "#{word}%")
+
+      elsif search == "backward_match"
+             @user = User.where("name LIKE?", "%#{word}")
+
+      elsif search == "perfect_match"
+             @user = User.where("name LIKE?", "#{word}")
+
+      elsif search == "partial_match"
+             @user = User.where("name LIKE?", "%#{word}%")
+
+      else
+             @user = User.all
+      end
+  end
+
+
 
   attachment :profile_image, destroy: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   resources :users do
   	get :followings, on: :member
     get :followers, on: :member
   end
-  
+
   resources :relationships, only: [:create, :destroy]
-  
+
   resources :books do
   	resources :book_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]
   end
-  
+
+  get 'search' => 'searches#search'
+
   root 'home#top'
+
   get 'home/about'
+
 end
